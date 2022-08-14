cef400b2aaaa4001993c65f03260afc04d2c7658
SHAJAL987 <ashiqulislamshajal.cse@gmail.com>
2022-08-14 17:27:14 +0600

    feat: final submit

--- src/service/user-management/user-creation.service.ts
+++ src/service/user-management/user-creation.service.ts
@@ -3,16 +3,13 @@
 import { connectionPool } from '@config/ormconfig';
 import { User } from '@database/entity/user-management/mms_user.entity';
 import BaseService from '@global/base.interface';
-// import { ICreateUser } from '@types/user-management/user-creation.interface';
+import { ICreateUser } from '../../types/user-management/user-creation.interface';
 import { Service } from 'typedi';
 import { EntityManager } from 'typeorm';
 
 @Service()
 export default class UserCreationService implements BaseService {
   constructor() {}
-  create(...args: any[]) {
-    throw new Error('Method not implemented.');
-  }
 
   //get all User List
   async get(tem?: EntityManager): Promise<User[]> {
@@ -22,9 +19,12 @@ export default class UserCreationService implements BaseService {
   }
 
   //create User
-  //   async create(data: ICreateUser, tem?: EntityManager): Promise<User>{
-  //     connectionPool.getRepository(User).create({
+  async create(data: ICreateUser, tem?: EntityManager): Promise<User> {
+    let newUser = await connectionPool.getRepository(User).create({
+      ...data,
+    });
 
-  //     })
-  //   }
+    newUser = await connectionPool.getRepository(User).save(newUser);
+    return newUser;
+  }
 }
